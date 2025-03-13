efd621273cdb6fe07dbfbf2d57c15fcb0ab06ba3
chinaza g ogwudiegwu <ogwudiegwuc@gmail.com>
2025-03-13 09:24:27 +0000

    still making response yoke

--- src/response.h
+++ src/response.h
@@ -2,31 +2,36 @@
 #define RESPONSE_H
 
 #include <string>
-
+#include <map>
 
 
 class Response{
-    public:
+        public:
 
-        //Constroctor
-        Response();
+            //Constroctor
+            Response();
 
-        // setting status
-        void setStatusMessage(const std::string& status_message);
-        void setStatusCode(const std::string& status_code );
-        // setting headers
-        void setHeaders(const std::string& header_name, const std::string& header_value);
-        //  setting body
-        void setBody(const std::string& body);
+            // setting status
+            void setStatusMessage(const std::string& status_message);
+            void setStatusCode(const std::string& status_code );
+            // setting headers
+            void setHeaders(const std::string& header_name, const std::string& header_value);
+            //  setting body
+            void setBody(const std::string& body);
 
+            //validator
+            bool validator() const;
+            // generating full response
+            std::string generateFullResponse() const;
 
-
-    private:
-        std::string status_code;
-        std::string status_message;
-        // body
-        //headers
-};
+        private:
+            std::string status_code;
+            std::string status_message;
+            // body
+            std::string body;
+            //headers
+            std::map<std::string , std::string> headers;
+    };
 
 
 
